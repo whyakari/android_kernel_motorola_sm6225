b72f7197e8767cbfee23490009df45e020353e57
shoiya <barryofc11@gmail.com>
2025-04-18 06:21:28 +0000

    fs: notify: fix variable not used. xD
    Signed-off-by: shoiya <barryofc11@gmail.com>

--- fs/notify/fdinfo.c
+++ fs/notify/fdinfo.c
@@ -139,7 +139,6 @@ out_seq_printf:
 		 * least one bit (FS_EVENT_ON_CHILD) which is
 		 * used only internally to the kernel.
 		 */
-		u32 mask = mark->mask & IN_ALL_EVENTS;
 		seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:%x ",
 			   inode_mark->wd, inode->i_ino, inode->i_sb->s_dev,
 			   inotify_mark_user_mask(mark));
